77a596bca5ea32ff2b83a40e33df58ba35696687
yazan alrifae <142574359+yazanrifae22@users.noreply.github.com>
2025-12-23 01:37:47 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
+import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
   return {
     base: '/smart-serivce/',
+    publicDir: 'public',
     server: {
       port: 3000,
       host: '0.0.0.0',
